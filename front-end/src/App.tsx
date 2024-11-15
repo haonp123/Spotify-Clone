d707fb7c2e383e128f96b092ff2562baac34f20b
NoPro <haonp123@gmail.com>
2024-11-15 19:37:35 +0700

    FE: add mainLayout - sideBar

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,19 +1,24 @@
 import { Routes, Route } from "react-router-dom";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 
 import HomePage from "./pages/home/HomePage";
+import ChatPage from "./pages/chat/ChatPage";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
-import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layouts/MainLayout";
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route
           path="/sso-callback"
           element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl="/auth-callback" />}
         />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Route>
       </Routes>
     </>
   );
---
+++ front-end/src/layouts/components/RightSidebar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RightSidebar = () => {
+  return <div>Left Sidebar</div>;
+};
+
+export default RightSidebar;
